aa3c02dce8e27d7dda85f229188c18865e1219d3
Abdelkhalekmohamed <abdelkhalekmohamed137@gmail.com>
2024-06-10 20:23:56 +0200

    updated vulnerabilities

---
+++ repo_files/vulnerabilities/insecure_storage.py
@@ -0,0 +1,9 @@
+import hashlib
+
+def store_password(password):
+    # High severity, medium confidence issue (insecure cryptographic storage)
+    hashed_password = hashlib.md5(password.encode()).hexdigest()  # Using MD5 for hashing, which is insecure
+    print(f"Storing password: {hashed_password}")
+
+if __name__ == "__main__":
+    store_password("SuperSecretPassword123!")
