7a1757d10549fd2acfd1fc43353daf58070e30bd
Abdelkhalekmohamed <abdelkhalekmohamed137@gmail.com>
2024-06-11 11:32:45 +0200

    add command_injection.py file

---
+++ repo_files/command_injection.py
@@ -0,0 +1,13 @@
+import pickle
+
+
+def insecure_deserialize(data):
+    # High severity, low confidence issue (insecure deserialization)
+    obj = pickle.loads(data)  # Deserializing potentially unsafe data
+    print(f"Deserialized object: {obj}")
+
+
+if __name__ == "__main__":
+    serialized_data = pickle.dumps({"key": "value"})
+    insecure_deserialize(serialized_data)
+
